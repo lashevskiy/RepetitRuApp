ec1cc86008648908793eb84c86a6fc6b8b992a53
Artem Lashevskiy <a.lashevskiy@semrush.com>
2021-03-09 16:09:12 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { BackHandler, Platform, StyleSheet, } from 'react-native';
+import Constants from 'expo-constants';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+    WEBVIEW_REF = React.createRef();
+
+    state = {
+        canGoBack: false,
+    };
+
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+    }
+
+    handleBackButton = () => {
+        if (this.state.canGoBack) {
+            this.WEBVIEW_REF.current.goBack();
+            return true;
+        }
+    };
+
+    onNavigationStateChange = (navState) => {
+        this.setState({
+            canGoBack: navState.canGoBack,
+        });
+    };
+
+    render() {
+        return (
+            <WebView
+                source={{ uri: 'https://lashevskiy.github.io/RepetitRu/?&expo_app=true&v=11' }}
+                ref={this.WEBVIEW_REF}
+                onNavigationStateChange={this.onNavigationStateChange}
+                style={styles.container}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+    },
 });
